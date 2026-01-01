3dc119cacec950bdb6f2df0f12597d58a6a03499
Előd Farkas <farkaselod650@gmail.com>
2026-01-01 20:47:48 +0100

    Fix deserialize and added try-catch

--- Booker.ApiCaller/ApiCallerBase.cs
+++ Booker.ApiCaller/ApiCallerBase.cs
@@ -3,52 +3,60 @@
 internal static class ApiCallerBase
 {
     private const string APPLICATION_JSON = "application/json";
+    private static JsonSerializerOptions jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-    internal static async Task<T> SendAsync<T>(ApiRequest request)
+    internal static async Task<T?> SendAsync<T>(ApiRequest request)
         where T : class
     {
-        HttpClient _httpClient = new HttpClient();
-
-        HttpRequestMessage message = new(request.Method, request.Url);
-        message.Headers.Add("Accept", APPLICATION_JSON);
-
-        if (request.Data is not null)
+        try
         {
-            message.Content = new StringContent(
-                JsonSerializer.Serialize(request.Data),
-                Encoding.UTF8,
-                APPLICATION_JSON
-            );
-        }
+            HttpClient _httpClient = new HttpClient();
 
-        HttpResponseMessage responseMessage = await _httpClient.SendAsync(message);
+            HttpRequestMessage message = new(request.Method, request.Url);
+            message.Headers.Add("Accept", APPLICATION_JSON);
 
-        string? errorMessage = null;
-
-        if (responseMessage.IsSuccessStatusCode)
-        {
-            switch (responseMessage.StatusCode)
+            if (request.Data is not null)
             {
-                case HttpStatusCode.NotFound:
-                    errorMessage = "The url is invalid";
-                    break;
-                case HttpStatusCode.Unauthorized:
-                    errorMessage = "Unauthorized";
-                    break;
-                case HttpStatusCode.Forbidden:
-                    errorMessage = "Forbidden";
-                    break;
-                default:
-                    break;
+                message.Content = new StringContent(
+                    JsonSerializer.Serialize(request.Data),
+                    Encoding.UTF8,
+                    APPLICATION_JSON
+                );
             }
 
-            if (errorMessage is not null)
-            {
-                throw new ApiCallerException(errorMessage);
-            }
-        }
+            HttpResponseMessage responseMessage = await _httpClient.SendAsync(message);
 
-        var responseContent = await responseMessage.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<T>(responseContent);
+            string? errorMessage = null;
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                switch (responseMessage.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        errorMessage = "The url is invalid";
+                        break;
+                    case HttpStatusCode.Unauthorized:
+                        errorMessage = "Unauthorized";
+                        break;
+                    case HttpStatusCode.Forbidden:
+                        errorMessage = "Forbidden";
+                        break;
+                    default:
+                        break;
+                }
+
+                if (errorMessage is not null)
+                {
+                    throw new ApiCallerException(errorMessage);
+                }
+            }
+
+            var responseContent = await responseMessage.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseContent, jsonOptions);
+        }
+        catch (Exception ex)
+        {
+            throw new ApiCallerException("An unhandled exception occured.");
+        }
     }
 }
