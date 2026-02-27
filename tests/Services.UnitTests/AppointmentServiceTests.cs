fdd2f3004f372da124dd377768ef87a1908b3392
Előd Farkas <farkaselod650@gmail.com>
2026-02-27 16:20:37 +0100

    AppointmentService tests

---
+++ tests/Services.UnitTests/AppointmentServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using Booker.Models.DTOs;
+
+namespace Services.UnitTests;
+
+public class AppointmentServiceTests
+{
+    private AppointmentService appointmentService = null!;
+    private IAppointmentRepository appointmentRepository = null!;
+
+    [Before(Test)]
+    public void SetUp()
+    {
+        SetUpRepository();
+        var mapper = SetUpMapper();
+
+        appointmentService = new AppointmentService(appointmentRepository, mapper);
+    }
+
+    [Test]
+    public async Task GetAppointments_ShouldReturnDTOs()
+    {
+        var result = await appointmentService.GetAppointments(1);
+
+        await Assert.That(result.Count).IsEqualTo(2);
+    }
+
+    [Test]
+    public async Task AddAppointment_ShouldPass()
+    {
+        var newAppointment = Substitute.For<AppointmentDto>();
+
+        await appointmentService.AddAppointment(newAppointment);
+
+        await appointmentRepository.Received(1).AddAppointmentAsync(Arg.Any<Appointment>());
+    }
+
+    private void SetUpRepository()
+    {
+        appointmentRepository = Substitute.For<IAppointmentRepository>();
+
+        appointmentRepository
+            .GetAppointmentsForCalendarAsync(Arg.Any<int>(), Arg.Any<CancellationToken>())
+            .Returns(callInfo =>
+            {
+                var calendarId = callInfo.ArgAt<int>(0);
+                return Task.FromResult(
+                    AppointmentTestData.Appointments.Where(a => a.CalendarId == calendarId).ToList()
+                );
+            });
+    }
+
+    private IMapper SetUpMapper()
+    {
+        var mapper = Substitute.For<IMapper>();
+
+        mapper
+            .Map<List<AppointmentDto>>(Arg.Any<List<Appointment>>())
+            .Returns(callInfo =>
+            {
+                var entities = callInfo.ArgAt<List<Appointment>>(0);
+                var dtos = new List<AppointmentDto>();
+
+                foreach (var entity in entities)
+                {
+                    dtos.Add(
+                        new AppointmentDto
+                        {
+                            Id = entity.Id,
+                            CalendarId = entity.CalendarId,
+                            ServiceId = entity.ServiceId,
+                            StartTime = entity.StartTime,
+                            EndTime = entity.EndTime,
+                            IsReadonly = entity.IsReadonly,
+                        }
+                    );
+                }
+
+                return dtos;
+            });
+
+        return mapper;
+    }
+}
---
+++ tests/Services.UnitTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using AutoMapper;
+global using Booker.Repository.Entities;
+global using Booker.Repository.Repositories.Interfaces;
+global using Booker.Services.Services;
+global using NSubstitute;
+global using Services.UnitTests.TestData;
